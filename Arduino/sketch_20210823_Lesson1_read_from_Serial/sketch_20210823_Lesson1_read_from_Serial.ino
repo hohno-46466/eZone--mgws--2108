ff3d18b4620ff3cbc4c40610c59fa49eb5d790a2
hohno-46466 <hohno.46466@gmail.com>
2021-08-26 09:14:03 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

---
+++ Arduino/sketch_20210823_Lesson1_read_from_Serial/sketch_20210823_Lesson1_read_from_Serial.ino
@@ -0,0 +1,41 @@
+//Button to turn ON/OFF LED
+
+//Constants won't change. They're used here to set pin numbers:
+const int buttonPin = 6; // the number of the pushbutton pin
+const int ledPin = 4; // the number of the LED pin
+// variables will change:
+
+int buttonState = 0; // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  //
+  Serial.begin(57600);
+}
+
+void loop() {
+  char *keyword;
+  long timenifo;
+  int val;
+  // read the state of the pushbutton value:
+  if (Serial.available() > 0) {
+    String str = Serial.readStringUntil('\n');
+    sscanf(str.c_str, "%s %;d %d", keyword, &timeinfo, &buttonState);
+  } else {
+    buttonState = 0;
+  }
+
+  buttonState = buttonState | digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
